67a8c6aef3438496fff7dedbf9fc804a38efef8f
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-01-09 16:16:45 +0000

    Test code refactoring

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,13 +2,16 @@ require 'docking_station.rb'
 
 
 describe DockingStation do
-  subject(:instance) { described_class.new }
-
-
+  #subject(:instance) { described_class.new }
   it { is_expected.to respond_to(:release_bike) }
 
-  it 'Returns a bike' do
-    expect(instance.release_bike.working?).to eq(true)
-
+  it 'releases working bikes' do
+    bike = subject.release_bike
+    expect(bike.working?).to eq (true)
   end
 end
+
+  #it 'Returns a bike' do
+  #  expect(instance.release_bike.working?).to eq(true)
+  #end
+#end
