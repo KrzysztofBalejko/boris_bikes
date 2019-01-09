db717daae142275e3723d5b540fd95a0f5ffffb2
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-01-09 22:31:09 +0000

    Test: return the bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -15,6 +15,10 @@ describe DockingStation do
     expect(subject.dock(bike)).to eq bike
   end
 
-  it { is_expected.to respond_to(:bike) }
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike 
+  end
 
 end
