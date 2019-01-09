e41dbdc717d4c6e9e8b3ae2f6d85dedbe00d07fb
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-01-09 16:04:12 +0000

    Test: releasing working bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,11 +1,9 @@
+require_relative 'bike'
+
 class DockingStation
 
   def release_bike
-
+    bike = Bike.new
+    return bike
   end
-
-
-
-
-
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,14 @@
-require 'docking_station'
+require 'docking_station.rb'
+
 
 describe DockingStation do
+  subject(:instance) { described_class.new }
+
+
   it { is_expected.to respond_to(:release_bike) }
+
+  it 'Returns a bike' do
+    expect(instance.release_bike.working?).to eq(true)
+
+  end
 end
