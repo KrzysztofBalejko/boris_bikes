7a95042dcc1d52bbdc9516ae8fc4b03e79dc3d7d
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-01-10 15:20:08 +0000

    Test: error if station is full

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -25,4 +25,15 @@ RSpec.describe DockingStation do
     docking_station = DockingStation.new
     expect { docking_station.release_bike }.to raise_error('no bikes!')
   end
+
+  it 'raises an error if station is full' do
+    docking_station = DockingStation.new
+    bike = Bike.new
+    docking_station.dock(bike)
+    expect { docking_station.dock(bike) }.to raise_error('Rack is full!')
+  end
+
+
+
+
 end
