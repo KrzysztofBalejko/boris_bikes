a1aba77997f36ff74bac79a8d42a165e6dd190b5
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-01-10 14:58:13 +0000

    Test: error if station is empty

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,14 +1,14 @@
 require 'docking_station.rb'
 
 
-describe DockingStation do
+RSpec.describe DockingStation do
 
   it { is_expected.to respond_to(:release_bike) }
 
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike.working?).to eq (true)
-  end
+  # it 'releases working bikes' do
+  #   bike = subject.release_bike
+  #   expect(bike.working?).to eq (true)
+  # end
 
   it 'dock a bike' do
     bike = Bike.new
@@ -18,7 +18,11 @@ describe DockingStation do
   it 'returns docked bikes' do
     bike = Bike.new
     subject.dock(bike)
-    expect(subject.bike).to eq bike 
+    expect(subject.bike).to eq bike
   end
 
+  it 'raises an error if station is empty' do
+    docking_station = DockingStation.new
+    expect { docking_station.release_bike }.to raise_error('no bikes!')
+  end
 end
